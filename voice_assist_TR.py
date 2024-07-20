1cc9e3c9d121b6da2d0a55b093e060702da8abd1
HUmutI <huseyinumut2004@gmail.com>
2024-07-20 13:42:55 +0300

    openweather, spotify and chrome integrated to the voice_assist_TR

--- voice_assist_TR.py
+++ voice_assist_TR.py
@@ -4,6 +4,51 @@ import winsound
 from pydub import AudioSegment
 import pyautogui
 import webbrowser
+import spotipy 
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+clientID = os.getenv("CLIENT_ID")
+clientSecret = os.getenv("CLIENT_SECRET")
+username = os.getenv("USERNAME")
+weather_api_key = os.getenv("WEATHER_API_KEY")
+
+redirect_uri = 'http://google.com/callback/'
+scope = 'user-modify-playback-state user-read-playback-state user-read-currently-playing'
+oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri,scope=scope) 
+token_dict = oauth_object.get_access_token() 
+token = token_dict['access_token'] 
+spotifyObject = spotipy.Spotify(auth=token) 
+user_name = spotifyObject.current_user() 
+
+openweather_conditions_en = ["clear sky","few clouds","scattered clouds","broken clouds",
+                             "shower rain","rain","thunderstorm","snow","mist"]
+
+openweather_conditions_tr = ["açık gökyüzü", "az bulutlu", "parçalı bulutlu", "çok bulutlu", 
+                            "sağanak yağmur", "yağmurlu", "gök gürültülü fırtına", "karlı", "sisli"]
+def get_weather(city):
+    base_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}"
+    response = requests.get(base_url)
+    data = response.json()
+    print(data)
+    if response.status_code == 200:
+        main = data["main"]
+        weather = data["weather"][0]
+        temperature = int(main["temp"])
+        weather_description = weather["description"]
+        for i in range(len(openweather_conditions_en)):
+            if openweather_conditions_en[i] == weather_description:
+                weather_description = openweather_conditions_tr[i]
+                
+        weather_response = f"{weather_description} ve sıcaklık {round((temperature-272.15),1)} derece."
+        return weather_response
+    elif data["cod"] == "404":
+        return "Şehir bulunamadı. Lütfen tekrar deneyin."
+    else:
+        return "Hava durumu bilgisi alınamadı. Lütfen daha sonra tekrar deneyin."
 
 def listen_for_command():
     recognizer = sr.Recognizer()
@@ -26,18 +71,14 @@ def listen_for_command():
 
 def respond(response_text):
     print(response_text)
-    tts = gTTS(text=response_text, lang='tr')
+    tts = gTTS(text=response_text, lang='tr',slow=False)
     tts.save("response.mp3")
     sound = AudioSegment.from_mp3("response.mp3")
-    sound.export("response.wav", format="wav")
+    speedup_sound = sound.speedup(playback_speed=1.2)
+    speedup_sound.export("response.wav", format="wav")
     winsound.PlaySound("response.wav", winsound.SND_FILENAME)
-
-tasks = []
-listeningToTask = False
-
+    
 def main():
-    global tasks
-    global listeningToTask
     ss_count = 1
     while True:
         command = listen_for_command()
@@ -45,27 +86,55 @@ def main():
         triggerKeyword = "alper"
 
         if command and triggerKeyword in command:
-            if listeningToTask:
-                tasks.append(command)
-                listeningToTask = False
-                respond(f"{command} görev listenize eklendi. Şu anda listenizde {len(tasks)} görev var.")
-            elif "görev ekle" in command:
-                listeningToTask = True
-                respond("Tabii, görev nedir?")
-            elif "görevleri listele" in command:
-                respond("Tabii. Görevleriniz şunlar:")
-                for task in tasks:
-                    respond(task)
+            
+            if "haber" in command or "nasılsın" in command or "yapıyosun" in command:
+                respond("İyiyim, sen nasılsın?")
+                
             elif ("ekran görüntüsü al" in command) or ("ss al" in command):
                 pyautogui.screenshot(f"screenshot{ss_count}.png")
                 ss_count += 1
                 respond("Ekran görüntüsü alındı.")
+                
             elif "chrome" in command and "aç" in command:
                 webbrowser.open("https://www.youtube.com/watch?v=JTk5pxI5loY")
                 respond("Chrome'u açıyorum.")
+                
+            elif "spotify" in command and "aç" in command:
+                respond("Hangi şarkıyı çalmamı istersin?")
+                search_song = listen_for_command()
+                if "drake" in search_song:
+                    search_song = "Not Like Us"
+                results = spotifyObject.search(search_song, 1, 0, "track") 
+                songs_dict = results['tracks'] 
+                song_items = songs_dict['items'] 
+                song = song_items[0]['external_urls']['spotify'] 
+                respond("şarkı açılıyor")
+                webbrowser.open(song)
+            elif "şarkı" in command and "dur" in command:
+                spotifyObject.pause_playback()
+                respond("Şarkı durduruldu.")
+            elif "şarkı" in command and "devam" in command:
+                spotifyObject.start_playback()
+                respond("Devam ediyor.")
+            elif "arama yap" in command:
+                respond("Ne aramak istersiniz?")
+                search_query = listen_for_command()
+                if search_query:
+                    url = f"https://www.google.com/search?q={search_query}"
+                    respond("Arama yapılıyor")
+                    webbrowser.open(url)
+                    
+            elif "hava durumu" in command:
+                respond("Hangi şehrin hava durumunu öğrenmek istersiniz?")
+                city = listen_for_command()
+                if city:
+                    weather_info = get_weather(city)
+                    respond(weather_info)
+                    
             elif "çıkış" in command:
                 respond("Hoşça kal!")
                 break
+            
             else:
                 respond("Anlayamadım.")
 
