34e99339be40d45318de58e0a93d5b194c4d0df0
HUmutI <huseyinumut2004@gmail.com>
2024-07-20 11:53:36 +0300

    deleted unnecessary files and now time to combine assistant and spotify api

---
+++ playing_song_spotify.py
@@ -0,0 +1,35 @@
+import json 
+import spotipy 
+import webbrowser 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+clientID = os.getenv("CLIENT_ID")
+clientSecret = os.getenv("CLIENT_SECRET")
+username = os.getenv("USERNAME")
+
+redirect_uri = 'http://google.com/callback/'
+oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri) 
+token_dict = oauth_object.get_access_token() 
+token = token_dict['access_token'] 
+spotifyObject = spotipy.Spotify(auth=token) 
+user_name = spotifyObject.current_user() 
+  
+while True: 
+    print("Welcome to the project, " + user_name['display_name']) 
+    print("0 - Exit the console") 
+    print("1 - Search for a Song") 
+    user_input = int(input("Enter Your Choice: ")) 
+    if user_input == 1: 
+        search_song = input("Enter the song name: ") 
+        results = spotifyObject.search(search_song, 1, 0, "track") 
+        songs_dict = results['tracks'] 
+        song_items = songs_dict['items'] 
+        song = song_items[0]['external_urls']['spotify'] 
+        webbrowser.open(song) 
+        print('Song has opened in your browser.') 
+    elif user_input == 0: 
+        print("Good Bye, Have a great day!") 
+        break
+    else: 
+        print("Please enter valid user-input.") 
